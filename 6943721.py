fdc1c3644b567ee278a1d6b8adabc99d8045a84f
okaihenry <125649979+okaihenry@users.noreply.github.com>
2023-02-24 01:53:53 -0800

    Add files via upload
    Assignment Five(5)

--- 6943721.py
+++ 6943721.py
@@ -1,145 +1,63 @@
-"""
-M = w(-6x^2 + 6Lx - L^2)/12
-
-V = w(L/2 - x)
-
-where M = bending moment
-V = shear force
-L = the span of the beam
-x = a specified distance
-w = Load intensity
-"""
-
-#Question a
-#Print the bending moment and shear force at the ends (that is x = 0 and x = L)
-
-#Span of the beam 
-L = 12                                  #in meters
-
-#Load intensity
-w = 10                                  #in kN/m
-
-
-#At x = 0
-x = 0
-
-#Formula for the Bending Moment
-M = w*(-6*x**2 + 6*L*x  - L**2)/12
-
-#Formula for the Shear Force
-V = w*(L/2 - x)
-print("The bending moment at x =", x, "is ", M,"kNm")
-print("The shear force at x =", x, "is ", V,"kN" )
-print()
-
-
-#At x = L
-x = L
-
-#Formula for the Bending Moment
-M = w*(-6*x**2 + 6*L*x  - L**2)/12
-
-#Formula for the Shear Force
-V = w*(L/2 - x)
-print("The bending moment at x =", x, "is", M,"kNm")
-print("The shear force at x =", x, "is", V,"kN")
-print()
-
-
-#Question b 
-#Write an expression to estimate the distance at which the bending moment will be zero
-
-#Using the method of completing square x = L*sqrt(3)/6   + L/2 or -L*sqrt(3)/6 + L/2
-
-# Importing the sqrt function from the math module
-from math import sqrt 
-x1 = (L*sqrt(3))/6 + L/2
-x2 = (-L*sqrt(3))/6 + L/2
-print("The distance at which the Bending Moment will be zero is",str(x1)+"m", "or",str(x2)+"m")
-print()
-
-
-
-#Question c
-#Also determine the point at which the bending moment will be zero
-
-#The shear force will be zero at x = L/2
-distance = L/2 
-print("The shear force occurs at the point at which x =", str(distance)+" m")
-print()
-
-
-
-#Question d
-"""
-Use Numpy array to discretize a span of L = 12m, at a step of 10mm. Using the 
-initialized variable, evaluate the bending moment at each step in the array
-"""
-# Importing the numpy library
-import numpy as np
-
-#NOTE : 10mm is equivalent to 0.01m so the step would be 0.01
-
-#Using the arange function from numpy to generate an array of steps of 0.01 on the span length
-span_steps = np.arange(0, L + 0.01, 0.01)
-
-#Bending Moment at each step
-BM_each_step = w*(-6*span_steps**2 + 6*L*span_steps  - L**2)/12
-
-print("Bending Moment at each step: ",BM_each_step,"in kNm")
-print()
-
-
-
-
-#Question e 
-
-#Also calculate the shear force for each step, along the span
-Shear_Force_each_step = w*(L/2 - span_steps)
-
-print("Shear Force at each step :",Shear_Force_each_step,"in kN")
-print()
-abs_BM = np.abs(BM_each_step)
-
-#Question f 
-#Find points along L at which the absolute values of the bending moment array in d is minimum
-
-#Absolute values of the bending moment in (d)
-
-#Points along L at which the absolute values of the bending moment is minimum
-L_BM_min = span_steps[abs_BM == abs_BM.min()]
-print(L_BM_min[0], "in metres")
-print()
-
-
-
-#Question g 
-# Determine the relative errors between the estimated points of contra-flexure in (b) and (f)
-
-"""
-Relative Error = (|Expected - Actual|/ Actual Value) * 100%
-Expected = Distances(x1, x2) at which the Bending Moment will be minimum in (b) 
-Actual Value = The value we had in question (f) i.e. Point along L at which the BM is minimum in (f)
-"""
-
-#Converting the distances to an array
-Expected = np.array([x1, x2])
-Actual = L_BM_min
-
-#Relative errors in percent
-Relative_Errors = ((np.abs(Expected - Actual))/Actual) * 100
-print("Relative Errors :",Relative_Errors, "in percent(%)")
-print()
-
-
-
-#Question (h)
-#Find the points at which the maximum and minimum bending moment occur, using the discretized ponts 
-
-#Points along the discretized points at which the maximum bending moment occur 
-print("Points along wich the bending moment is maximum :",span_steps[ BM_each_step == BM_each_step.max()][0],"in metres")
-
-#Points along the discretized points at which the minimum bending moment occur 
-print("Points along wich the bending moment is minimum :",span_steps[ BM_each_step == BM_each_step.min()][0],"in metres")
- 
-#github link https://github.com/okaihenry/computer-programming-
+# list of available cars and their prices at Henry Motors
+cars = {
+'Lamborghini Aventador' : 89948,
+'Lamborghini Urus' : 70004,
+'Bugatti Chiron' : 88765,
+'Bugatti Divo' : 98655,
+'Ferrari 250 GTO' : 298939,
+'Ferrari 599 GTO' : 469903,
+'Toyota Landcruiser' : 45637,
+'Toyota Corolla' : 3894,
+'Toyota Rav4': 5800,
+'Toyota Prado' : 34700,
+'Lexus Lx570' : 68000,
+'Lexus Gx 460' : 78300,
+'Mazda 3' : 56000,
+'Mazda 6' : 83400,
+'Ford Focus' : 5700,
+'Ford Explorer' : 7600,
+'Ford Escape' : 3608,
+'Jeep Wrangler' : 65000,
+'Jeep Compass' : 69500,
+'Jeep Grand Cherokee' : 57000,
+'Chevrolet Cruze' : 48000,
+'Chevrolet Equinox' : 56000,
+'Chevrolet Camaro' : 97006,
+'Tesla Cybertruck' : 89000,
+'Tesla Model S' : 47600,
+'Tesla Model Y' : 52000,
+'Hyundai Elantra' : 47000,
+'Hyundai Tucson' : 36000,
+'Hyundai Accent' : 28000,
+'Audi R8' : 56000,
+'Audi A3' : 74000,
+'Audi Q5' : 66000,
+'Mitsubishi Pajero' : 20000,
+'Mitsubishi Outlander' : 15000,
+'Honda Civic' : 26500,
+'Honda Accord' : 46000,
+'Honda CR-V' : 35000,
+'Honda Pilot' : 38000,
+'Bentley Bentayga' :470000,
+'Land rover Defender' : 50000,
+'Range Rover Sport' : 780000,
+'Range Rover Evoque' : 5650000,
+'Lincoln Navigator' : 70000,
+'Rolls-Royce Phantom' : 589000,
+'Rolls-Royce Cullinan' : 999000,
+'Rolls-Royce Ghost' : 680000,
+'Infiniti QX60' :34000,
+'Infiniti Q60' :29000,
+'BMW X3' : 55000,
+'BMW X4' : 60000,
+'BMW X3' : 55000,
+'BMW 530i' : 75000,
+}  
+carName = input("Enter Your Preferred Car's Name': ")
+if carName in cars:
+    print("Yes Your Car is Available")
+    carPrice = cars[carName]
+    print(f"The price of {carName} is ${carPrice}.")
+else:   
+    print(f"Sorry,{carName} is not available") 
+# Github link https://github.com/okaihenry/computer-programming-
